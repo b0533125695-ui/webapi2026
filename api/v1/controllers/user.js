d934753906cb8f52f4b633fdf8cbbbe4b5d68f6d
Moty Bretler <b0533125695@gmail.com>
2026-05-24 21:21:50 +0300

    full CRUD in t_user table

--- api/v1/controllers/user.js
+++ api/v1/controllers/user.js
@@ -1,21 +1,94 @@
+const mysqldb=require('../models/mysqldb');
 module.exports={
     getAll:(req,res)=>{
-    res.status(200).json({msg:`all users`});
+
+        const sql='SELECT * FROM t_user';
+        mysqldb.query(sql,(error,results,fields)=>{
+            if (error==null){
+               console.log(results);
+               return res.status(200).json(results);
+            }
+            else{
+                console.log(error);
+                return res.status(500).json(error);
+            }
+})
+    // res.status(200).json({msg:`all Users`});
 },
     getById:(req,res)=>{
-    const UserId=req.params.id;
-    res.status(200).json({msg:`got user id ${UserId}`});
+    const Userid=req.params.id;
+    const sql=`SELECT * FROM t_user WHERE Userid=${Userid}`;
+        mysqldb.query(sql,(error,results,fields)=>{
+            if (error==null){
+               console.log(results);
+               return res.status(200).json(results);
+            }
+            else{
+                console.log(error);
+                return res.status(500).json(error);
+            }
+        });
 },
     add:(req,res)=>{
-    //הוספת משתמש חדש
-    res.status(200).json({msg:`add a user`});
+    const Userid=req.params.id;
+    let data=req.body;
+    let arr=Object.keys(data);
+    let keys='';
+    let values='';
+    for (let i=0;i<arr.length;i++)
+    {
+        keys+=`${arr[i]},`;
+        values+=`'${data[arr[i]]}',`;
+    }
+    keys=keys.substring(0,keys.length-1);
+    values=values.substring(0,values.length-1);
+     let sql=`INSERT INTO t_user (${keys}) VALUES (${values})`;
+    mysqldb.query(sql,(error,results,fields)=>{
+        if (error==null){
+           console.log(results);
+            return res.status(200).json(results);
+     }
+        else{
+            console.log(error);
+            return res.status(500).json(error);
+            }
+        
+   });
 },
     update:(req,res)=>{
-    const UserId=req.params.id;//עדכון משתמש ע"פ הקוד משתמש
-    res.status(200).json({msg:`update user id  ${UserId}`});
+    const Userid=req.params.id;
+    let sql='UPDATE t_user SET ';
+    let data=req.body;
+    let arr=Object.keys(data);
+    for (let i=0;i<arr.length;i++)
+    {
+        sql+=`${arr[i]}='${data[arr[i]]}',`;
+    }
+    sql=sql.substring(0,sql.length-1);
+    sql+=` WHERE Userid=${Userid}`;
+    mysqldb.query(sql,(error,results,fields)=>{
+        if (error==null){
+           console.log(results);
+            return res.status(200).json(results);
+     }
+        else{
+            console.log(error);
+            return res.status(500).json(error);
+            }
+        });
 },
     delete:(req,res)=>{
-    const UserId=req.params.id;
-    res.status(200).json({msg:`delete user id  ${UserId}`});
+    const Userid=req.params.id;
+    const sql=`DELETE FROM t_user WHERE Userid=${Userid}`;
+        mysqldb.query(sql,(error,results,fields)=>{
+            if (error==null){
+               console.log(results);
+               return res.status(200).json(results);
+            }
+            else{
+                console.log(error);
+                return res.status(500).json(error);
+            }
+        });
 }
 }
