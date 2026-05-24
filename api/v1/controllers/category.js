b9d13c5127a651192e6c13e34c7795da01770aea
Moty Bretler <b0533125695@gmail.com>
2026-05-24 22:26:45 +0300

    full CRUD in t_category table

--- api/v1/controllers/category.js
+++ api/v1/controllers/category.js
@@ -1,21 +1,94 @@
+const mysqldb=require('../models/mysqldb');
 module.exports={
     getAll:(req,res)=>{
-    res.status(200).json({msg:`all Categories`});
+
+        const sql='SELECT * FROM t_category';
+        mysqldb.query(sql,(error,results,fields)=>{
+            if (error==null){
+               console.log(results);
+               return res.status(200).json(results);
+            }
+            else{
+                console.log(error);
+                return res.status(500).json(error);
+            }
+})
+    // res.status(200).json({msg:`all products`});
 },
     getById:(req,res)=>{
-    const cid=req.params.id;
-    res.status(200).json({msg:`got category id ${cid}`});
+    const catid=req.params.id;
+    const sql=`SELECT * FROM t_category WHERE catID=${catid}`;
+        mysqldb.query(sql,(error,results,fields)=>{
+            if (error==null){
+               console.log(results);
+               return res.status(200).json(results);
+            }
+            else{
+                console.log(error);
+                return res.status(500).json(error);
+            }
+        });
 },
     add:(req,res)=>{
-    //הוספת קטגוריה חדשה
-    res.status(200).json({msg:`add a category`});
+    const catid=req.params.id;
+    let data=req.body;
+    let arr=Object.keys(data);
+    let keys='';
+    let values='';
+    for (let i=0;i<arr.length;i++)
+    {
+        keys+=`${arr[i]},`;
+        values+=`'${data[arr[i]]}',`;
+    }
+    keys=keys.substring(0,keys.length-1);
+    values=values.substring(0,values.length-1);
+     let sql=`INSERT INTO t_category (${keys}) VALUES (${values})`;
+    mysqldb.query(sql,(error,results,fields)=>{
+        if (error==null){
+           console.log(results);
+            return res.status(200).json(results);
+     }
+        else{
+            console.log(error);
+            return res.status(500).json(error);
+            }
+        
+   });
 },
     update:(req,res)=>{
-    const cid=req.params.id;//עדכון קטגוריה ע"פ הקוד קטגוריה
-    res.status(200).json({msg:`update category id  ${cid}`});
+    const catid=req.params.id;
+    let sql='UPDATE t_category SET ';
+    let data=req.body;
+    let arr=Object.keys(data);
+    for (let i=0;i<arr.length;i++)
+    {
+        sql+=`${arr[i]}='${data[arr[i]]}',`;
+    }
+    sql=sql.substring(0,sql.length-1);
+    sql+=` WHERE catid=${catid}`;
+    mysqldb.query(sql,(error,results,fields)=>{
+        if (error==null){
+           console.log(results);
+            return res.status(200).json(results);
+     }
+        else{
+            console.log(error);
+            return res.status(500).json(error);
+            }
+        });
 },
     delete:(req,res)=>{
-    const cid=req.params.id;
-    res.status(200).json({msg:`delete category id  ${cid}`});
+    const catid=req.params.id;
+    const sql=`DELETE FROM t_category WHERE catid=${catid}`;
+        mysqldb.query(sql,(error,results,fields)=>{
+            if (error==null){
+               console.log(results);
+               return res.status(200).json(results);
+            }
+            else{
+                console.log(error);
+                return res.status(500).json(error);
+            }
+        });
 }
 }
