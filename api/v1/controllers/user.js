6e0069e51302d2fa19923bb52807b4602e0ad534
Moty Bretler <b0533125695@gmail.com>
2026-05-31 03:28:02 +0300

    Adding a query for login using a username and password, as well as encrypting the password in the update function

--- api/v1/controllers/user.js
+++ api/v1/controllers/user.js
@@ -1,94 +1,150 @@
-const mysqldb=require('../models/mysqldb');
-module.exports={
-    getAll:(req,res)=>{
+const bcrypt = require('bcrypt');
+const mysqldb = require('../models/mysqldb');
 
-        const sql='SELECT * FROM t_category';
-        mysqldb.query(sql,(error,results,fields)=>{
-            if (error==null){
-               console.log(results);
-               return res.status(200).json(results);
-            }
-            else{
-                console.log(error);
-                return res.status(500).json(error);
-            }
-})
-    // res.status(200).json({msg:`all Users`});
-},
-    getById:(req,res)=>{
-    const catid=req.params.id;
-    const sql=`SELECT * FROM t_category WHERE catid=${catid}`;
-        mysqldb.query(sql,(error,results,fields)=>{
-            if (error==null){
-               console.log(results);
-               return res.status(200).json(results);
-            }
-            else{
+module.exports = {
+    getAll: (req, res) => {
+        const sql = 'SELECT * FROM t_user';
+
+        mysqldb.query(sql, (error, results ) => {
+            if (error == null) {
+                console.log(results);
+                return res.status(200).json(results);
+            } else {
                 console.log(error);
                 return res.status(500).json(error);
             }
         });
-},
-    add:(req,res)=>{
-    const catid=req.params.id;
-    let data=req.body;
-    let arr=Object.keys(data);
-    let keys='';
-    let values='';
-    for (let i=0;i<arr.length;i++)
-    {
-        keys+=`${arr[i]},`;
-        values+=`'${data[arr[i]]}',`;
-    }
-    keys=keys.substring(0,keys.length-1);
-    values=values.substring(0,values.length-1);
-     let sql=`INSERT INTO t_category (${keys}) VALUES (${values})`;
-    mysqldb.query(sql,(error,results,fields)=>{
-        if (error==null){
-           console.log(results);
-            return res.status(200).json(results);
-     }
-        else{
-            console.log(error);
-            return res.status(500).json(error);
+    },
+
+    getById: (req, res) => {
+        const userID = req.params.id;
+        const sql = `SELECT * FROM t_user WHERE userID=${userID}`;
+        mysqldb.query(sql, (error, results ) => {
+            if (error == null) {
+                console.log(results);
+                return res.status(200).json(results);
+            } else {
+                console.log(error);
+                return res.status(500).json(error);
             }
+        });
+    },
+
+    add: (req, res) => {
+        const data = req.body;
+        const arr = Object.keys(data);
+        let keys = '';
+        let values = '';
+        let sql = `SELECT * FROM t_user WHERE email='${data.email}'`;
+        mysqldb.query(sql, (error, results ) => {
+            if (error != null) {
+                console.log(error);
+                return res.status(500).json(error);
+            } else if (results.length > 0) {
+                return res.status(200).json({ msg: 'User already exists' }); // המשתמש כבר קיים
+            }
+
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] === 'pass') {
+                    const pass = data[arr[i]];
+                    const hashPass = bcrypt.hashSync(pass, 10);
+                    keys += `${arr[i]},`;
+                    values += `'${hashPass}',`;
+                } else {
+                    keys += `${arr[i]},`;
+                    values += `'${data[arr[i]]}',`;
+                }
+            }
+
+            keys = keys.substring(0, keys.length - 1);
+            values = values.substring(0, values.length - 1);
+            sql = `INSERT INTO t_user (${keys}) VALUES (${values})`;
+
+            mysqldb.query(sql, (error, results ) => {
+                if (error == null) {
+                    console.log(results);
+                    return res.status(200).json(results);
+                } else {
+                    console.log(error);
+                    return res.status(500).json(error);
+                }
+            });
+        });
+    },
+
+    update: (req, res) => {
+        const userID = req.params.id;
+        let sql = 'UPDATE t_user SET ';
+        const data = req.body;
+        const arr = Object.keys(data);
         
-   });
-},
-    update:(req,res)=>{
-    const catid=req.params.id;
-    let sql='UPDATE t_category SET ';
-    let data=req.body;
-    let arr=Object.keys(data);
-    for (let i=0;i<arr.length;i++)
-    {
-        sql+=`${arr[i]}='${data[arr[i]]}',`;
-    }
-    sql=sql.substring(0,sql.length-1);
-    sql+=` WHERE catid=${catid}`;
-    mysqldb.query(sql,(error,results,fields)=>{
-        if (error==null){
-           console.log(results);
-            return res.status(200).json(results);
-     }
-        else{
-            console.log(error);
-            return res.status(500).json(error);
+        
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == 'pass') {
+                const pass = data[arr[i]];
+                const hashPass = bcrypt.hashSync(pass, 10);
+                sql += `${arr[i]}='${hashPass}',`;
             }
-        });
-},
-    delete:(req,res)=>{
-    const catid=req.params.id;
-    const sql=`DELETE FROM t_category WHERE catid=${catid}`;
-        mysqldb.query(sql,(error,results,fields)=>{
-            if (error==null){
-               console.log(results);
-               return res.status(200).json(results);
+            else {
+            sql += `${arr[i]}='${data[arr[i]]}',`; 
             }
-            else{
+        }
+
+        sql = sql.substring(0, sql.length - 1);
+        sql += ` WHERE userID=${userID}`;
+
+        mysqldb.query(sql, (error, results ) => {
+            if (error == null) {
+                console.log(results);
+                return res.status(200).json(results);
+            } else {
                 console.log(error);
                 return res.status(500).json(error);
             }
         });
-}
-}
+    },
+
+    delete: (req, res) => {
+        const userID = req.params.id;
+        const sql = `DELETE FROM t_user WHERE userID=${userID}`;
+
+        mysqldb.query(sql, (error, results ) => {
+            if (error == null) {
+                console.log(results);
+                return res.status(200).json(results);
+            } else {
+                console.log(error);
+                return res.status(500).json(error);
+            }
+        });
+    },
+
+    login: (req, res) => {
+        const data = req.body;
+        
+        let sql = `SELECT * FROM t_user WHERE email='${data.email}'`;
+        mysqldb.query(sql, (error, results ) => {
+            if (error != null) {
+                console.log(error);
+                return res.status(500).json({status:false,error: error.message, data: []});
+            } else if (results.length == 0)// המשתמש שנרשם לא קיים
+            {
+                return res.status(200).json({status:false,erroe:null, data: []});
+            }
+            let user = results[0];
+            bcrypt.compare(data.pass, user.pass, (err, same) => {
+                if (err!= null) {
+                    console.log(err);
+                    return res.status(500).json({status:false,error: err.message, data: []});
+                }
+                if (same==true) {
+                    return res.status(200).json({status:true,error: null, data: user});
+                }
+                else{
+                    return res.status(200).json({status:false,error: null, data: []});
+                }
+            });
+        });
+    },
+
+};
--- api/v1/routes/user.js
+++ api/v1/routes/user.js
@@ -5,5 +5,6 @@ router.get('/:id',userController.getById);
 router.delete('/:id',userController.delete);
 router.post('/',userController.add);
 router.put('/:id',userController.update);
+router.post('/login',userController.login);
 
 module.exports=router;
